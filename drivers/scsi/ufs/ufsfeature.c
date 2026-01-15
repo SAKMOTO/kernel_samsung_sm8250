93ae43e739dd89c86cfc2a94e4a5255feb5b23cf
techyguyperplexable <objecting@objecting.org>
2026-01-15 22:43:24 +0530

    scsi: ufs: Fix compilation when CONFIG_UFSHPB is disabled
    Guard HPB code with CONFIG_UFSHPB and include linux/of.h.
    
    Change-Id: I530a31b7d2913a16a8afce1a21406225b6ecfcae
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- drivers/scsi/ufs/ufsfeature.c
+++ drivers/scsi/ufs/ufsfeature.c
@@ -35,6 +35,7 @@
  * license terms, and distributes only under these terms.
  */
 
+#include <linux/of.h>
 #include "ufsfeature.h"
 #include "ufshcd.h"
 
@@ -136,6 +137,7 @@ out:
 
 int ufsf_hpb_dt_check(struct ufs_hba *hba)
 {
+#if defined(CONFIG_UFSHPB)
         struct device_node *node = hba->dev->of_node;
         struct ufsf_feature *ufsf = &hba->ufsf;
         int dt_hpb_enable = 1;
@@ -186,6 +188,9 @@ int ufsf_hpb_dt_check(struct ufs_hba *hba)
 query_fail:
         pm_runtime_put_sync(hba->dev);
         return 0;
+#else
+        return 0;
+#endif
 }
 
 void ufsf_device_check(struct ufs_hba *hba)
